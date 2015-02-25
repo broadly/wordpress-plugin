736e21ad93552aa048bd65e6d38ed34e69aff7fc
Tyler Longren <tlongren@gmail.com>
2015-02-25 10:07:29 -0600

    Validate SSL certs.

--- broadly.php
+++ broadly.php
@@ -32,7 +32,11 @@ function get_broadly($atts) {
 
     if ( class_exists( 'WP_Http' ) ) {
 
-      $resp = wp_remote_get( $url );
+      $args = array(
+        'sslverify' => true
+      );
+
+      $resp = wp_remote_request( $url, $args );
 
       if ( 200 == $resp['response']['code'] ) {
 
@@ -64,42 +68,42 @@ add_shortcode('broadly', 'get_broadly');
  */
 function broadly_init() {
 
-	// Make plugin available for translation, change /languages/ to your .mo-files folder name
-	load_plugin_textdomain( 'broadly', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+  // Make plugin available for translation, change /languages/ to your .mo-files folder name
+  load_plugin_textdomain( 'broadly', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 
-	// Initialize Sunrise
-	$admin = new Sunrise6( array(
-			'file'       => __FILE__,
-			'slug'       => 'broadly',
-			'prefix'     => 'broadly_',
-			'textdomain' => 'broadly',
-			'css'        => '',
-			'js'         => ''
-		) );
+  // Initialize Sunrise
+  $admin = new Sunrise6( array(
+      'file'       => __FILE__,
+      'slug'       => 'broadly',
+      'prefix'     => 'broadly_',
+      'textdomain' => 'broadly',
+      'css'        => '',
+      'js'         => ''
+    ) );
 
   $plugin_dir = plugin_dir_url( __FILE__ );
 
-	// Prepare array with options
-	$options = array(
+  // Prepare array with options
+  $options = array(
 
-		// Open tab: Regular fields
-		array(
-			'type' => 'opentab',
-			'name' => __( 'Settings', 'broadly' )
-		),
+    // Open tab: Regular fields
+    array(
+      'type' => 'opentab',
+      'name' => __( 'Settings', 'broadly' )
+    ),
 
-		array(
-			'id'      => 'account_id',
-			'type'    => 'text',
-			'default' => '',
-			'name'    => __( 'Broadly Account ID', 'broadly' ),
-			'desc'    => __( 'The ID provided by your account manager', 'broadly' )
-		),
+    array(
+      'id'      => 'account_id',
+      'type'    => 'text',
+      'default' => '',
+      'name'    => __( 'Broadly Account ID', 'broadly' ),
+      'desc'    => __( 'The ID provided by your account manager', 'broadly' )
+    ),
 
-		// Close tab: Regular fields
-		array(
-			'type' => 'closetab'
-		),
+    // Close tab: Regular fields
+    array(
+      'type' => 'closetab'
+    ),
     // Open tab: Regular fields
     array(
       'type' => 'opentab',
@@ -115,17 +119,17 @@ function broadly_init() {
     array(
       'type' => 'closetab'
     )
-	);
+  );
 
-	// Add top-level menu (like Dashboard -> Comments)
-	$admin->add_menu( array(
-			'page_title'  => __( 'Broadly Settings', 'broadly' ), // Settings page <title>
-			'menu_title'  => __( 'Broadly Settings', 'broadly' ), // Menu title, will be shown in left dashboard menu
-			'capability'  => 'manage_options', // Minimal user capability to access this page
-			'slug'        => 'broadly-settings', // Unique page slug
-			'position'    => '91.1', // Menu position from 80 to <infinity>, you can use decimals
-			'options'     => $options // Array with options available on this page
-		) );
+  // Add top-level menu (like Dashboard -> Comments)
+  $admin->add_menu( array(
+      'page_title'  => __( 'Broadly Settings', 'broadly' ), // Settings page <title>
+      'menu_title'  => __( 'Broadly Settings', 'broadly' ), // Menu title, will be shown in left dashboard menu
+      'capability'  => 'manage_options', // Minimal user capability to access this page
+      'slug'        => 'broadly-settings', // Unique page slug
+      'position'    => '91.1', // Menu position from 80 to <infinity>, you can use decimals
+      'options'     => $options // Array with options available on this page
+    ) );
 }
 
 // Hook to plugins_loaded
