42eb4a04a6368dfea0ee8ad53688d5ad04a9c4c7
Josh Melick <josh@melick.us>
2015-09-30 08:51:34 -0700

    Better variable name for embed URL

--- broadly.php
+++ broadly.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Broadly for WordPress
-Description: Dynamic integration of your Broadly reviews within your existing WordPress website. 
+Description: Dynamic integration of your Broadly reviews within your existing WordPress website.
 Plugin URL: http://broadly.com
 Author: Broadly
 Author URI: http://broadly.com/
@@ -10,27 +10,27 @@ License: GPLv2 or later
 */
 
 if ( ! class_exists( 'Broadly_Plugin' ) ) {
-	
+
 	/**
 	 * Main Broadly Plugin class
-	 * 
-	 * Responsible for the frontend review management and backend  
+	 *
+	 * Responsible for the frontend review management and backend
 	 * settings for the plugin.
-	 * 
+	 *
 	 * @author nofearinc
 	 *
 	 */
 	class Broadly_Plugin {
 
 		public static $version = '2.0.1';
-	
+
 		function __construct() {
 			// Creating the admin menu
 			add_action( 'admin_menu', array( $this, 'broadly_menu' ) );
-			
+
 			// Register the Settings fields
 			add_action( 'admin_init', array( $this, 'broadly_settings_init' ) );
-			
+
 			// Replace the Broadly scripts with the prefetched HTML
 			add_filter( 'the_content', array( $this, 'replace_js' ) );
 		}
@@ -39,30 +39,30 @@ if ( ! class_exists( 'Broadly_Plugin' ) ) {
 		 * Register a menu page for Broadly under Settings
 		 */
 		public function broadly_menu() {
-			add_options_page( __('Broadly', 'broadly' ), 
+			add_options_page( __('Broadly', 'broadly' ),
 					__( 'Broadly Setup', 'broadly' ),
 					'manage_options', 'broadly', array( $this, 'broadly_menu_cb' ) );
 		}
-		
+
 		/**
 		 * Settings class initialization
 		 */
 		public function broadly_settings_init() {
 			include_once 'settings.class.php';
 		}
-		
+
 		/**
 		 * Menu page callback - render the UI form for the admin
 		 */
 		public function broadly_menu_cb() {
 			$broadly_options = get_option( 'broadly_options', array() );
-			
+
 			include_once 'settings-page.php';
 		}
 
 		/**
 		 * Replace the JS snippet with the prefetched reviews
-		 * 
+		 *
 		 * @param string $content the existing page content
 		 * @return string $content the updated page content if a script is found
 		 */
@@ -72,29 +72,29 @@ if ( ! class_exists( 'Broadly_Plugin' ) ) {
 
 			// Proceed further only if a match is found - false will handle both 0 and false
 			if ( false != $matches_count ) {
-				
+
 				// Iterate through all of the matches if more scripts are injected
 				for ( $current_match = 0; $current_match < $matches_count; $current_match++ ) {
-					
+
 					// Fetch the entire script and the data-url match
 					$script_match = $matches[0][$current_match];
 					$dataurl_match = $matches[1][$current_match];
-					
-					// Append the data-url and build the reviews URL
-					$broadly_reviews_url = 'http://embed.broadly.com/' . $dataurl_match;
-		
+
+					// Append the data-url and build the embed URL
+					$broadly_embed_url = 'http://embed.broadly.com/' . $dataurl_match;
+
 					$args = array();
 					/**
 					 * Hook the arguments for the remote call.
-					 * 
+					 *
 					 * If needed, we can disable SSL or update the other HTTP arguments.
 					 */
 					$args = apply_filters( 'broadly_ssl_args', $args );
-		
+
 					add_filter( 'http_request_args', array( $this, 'filter_broadly_headers' ), 10, 2 );
-					$response = wp_remote_get( $broadly_reviews_url, $args );
+					$response = wp_remote_get( $broadly_embed_url, $args );
 					remove_filter( 'http_request_args', array( $this, 'filter_broadly_headers' ), 10 );
-					
+
 					// Verify for errors - not being sent for reporting yet
 					$error = null;
 					if ( is_wp_error( $response ) ) {
@@ -106,7 +106,7 @@ if ( ! class_exists( 'Broadly_Plugin' ) ) {
 							$error = __( 'No body tag in the response', 'broadly' );
 						}
 					}
-					
+
 					// If errors occured, don't replace the script tags
 					if ( ! is_null( $error ) ) {
 						continue;
@@ -121,13 +121,13 @@ if ( ! class_exists( 'Broadly_Plugin' ) ) {
 					$content = str_replace( $script_match, $opening_comment . $embed_content . $closing_comment, $content );
 				}
 			}
-			
+
 			return $content;
 		}
 
 		/**
 		 * Filter Broadly Headers
-		 * 
+		 *
 		 * @param $request_args original request arguments
 		 * @param $url request URL
 		 */
@@ -152,7 +152,7 @@ if ( ! class_exists( 'Broadly_Plugin' ) ) {
 
 		/**
 		 * Parse the type of embed from the data_uri
-		 * 
+		 *
 		 * @param $data_uri argument passed to Broadly including the embed name
 		 */
 		private function parse_embed_from_datauri( $data_uri ) {
@@ -168,7 +168,7 @@ if ( ! class_exists( 'Broadly_Plugin' ) ) {
 			return $embed_name;
 		}
 	}
-	
+
 	// Initialize the plugin body
 	$broadly = new Broadly_Plugin();
 }
