3f35544636cf8b7fa2a39d5ab81715b24ccd67fb
Josh Melick <josh@melick.us>
2015-10-07 19:46:46 -0700

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -57,7 +57,7 @@ fi
 if git show-ref --tags --quiet --verify -- "refs/tags/$NEWVERSION1"
 	then
 		echo "Version $NEWVERSION1 already exists as git tag. Exiting...";
-		#exit 1;
+		exit 1;
 	else
 		echo "Git version does not exist. Let's proceed..."
 fi
@@ -69,7 +69,7 @@ svn co $SVNURL $SVNPATH
 if [ -d "$SVNPATH/tags/$NEWVERSION1" ]
 	then
 	  echo "Version $NEWVERSION1 already exists as SVN tag. Exiting...";
-	  #exit 1;
+	  exit 1;
 	else
 		echo "SVN version does not exist. Let's proceed..."
 fi
@@ -109,20 +109,20 @@ echo "Changing directory to SVN and committing to trunk"
 cd $SVNPATH/trunk/
 # Add all new files that are not set to be ignored
 svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2}' | xargs svn add
-#svn commit --username=$SVNUSER -m "$COMMITMSG"
+svn commit --username=$SVNUSER -m "$COMMITMSG"
 
 echo "Creating new SVN tag & committing it"
 cd $SVNPATH
 svn copy trunk/ tags/$NEWVERSION1/
 cd $SVNPATH/tags/$NEWVERSION1
-#svn commit --username=$SVNUSER -m "Tagging version $NEWVERSION1"
+svn commit --username=$SVNUSER -m "Tagging version $NEWVERSION1"
 
 echo "Tagging new version in git"
 cd $GITPATH
-#git tag -a "$NEWVERSION1" -m "Tagging version $NEWVERSION1"
-#git push origin master --tags
+git tag -a "$NEWVERSION1" -m "Tagging version $NEWVERSION1"
+git push origin master --tags
 
 echo "Removing temporary directory $SVNPATH"
-#rm -fr $SVNPATH/
+rm -fr $SVNPATH/
 
 echo "*** FIN ***"
