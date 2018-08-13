ee0f79763deecebb905b0ecf2e9aeb5775c93e3d
Lucas Florio <lucasefe@gmail.com>
2018-08-13 15:30:16 -0300

    Rewrite settings page

--- settings.class.php
+++ settings.class.php
@@ -18,34 +18,19 @@ class Broadly_Settings {
 		add_settings_section(
 				'broadly_admin_section',
 				null,
-				array( $this, 'broadly_settings_section_cb' ),
+				null,
 				'broadly'
 		);
 		
 		add_settings_field(
 				'broadly_account_id',
-				__( 'Broadly Account ID', 'broadly' ),
+				__( 'Business ID', 'broadly' ),
 				array( $this, 'broadly_account_id_cb' ),
 				'broadly',
 				'broadly_admin_section'
 		);
-
-		add_settings_field(
-			'broadly_webchat_enabled',
-			__( 'Webchat Enabled', 'broadly' ),
-			array( $this, 'broadly_webchat_enabled_cb' ),
-			'broadly',
-			'broadly_admin_section'
-		);
 	}
 	
-	/**
-	 * Settings message for the section heading
-	 */
-	public function broadly_settings_section_cb() {
-		_e( 'Please enter your Account ID here. Based on your Account ID we will display a table with the available'
-				.' script snippets that you could use across the site.', 'broadly' );
-	}
 	
 	/**
 	 * Broadly Account ID field management
@@ -60,37 +45,7 @@ class Broadly_Settings {
 			$account_id = esc_html( $broadly_options['broadly_account_id'] );
 		}
 		
-		echo "<input type='text' name='broadly_options[broadly_account_id]' value='$account_id'>";
-	}
-
-	/**
-	 * Broadly Webchat Enabled field management
-	 */
-	public function broadly_webchat_enabled_cb() {
-		$broadly_options = get_option( 'broadly_options', array() );
-		
-		$webchat_enabled = 'Yes';
-		
-		if ( is_array( $broadly_options ) && ! empty( $broadly_options['broadly_webchat_enabled'] ) ) {
-			$webchat_enabled = $broadly_options['broadly_webchat_enabled'];
-		}
-		
-		echo "<select name='broadly_options[broadly_webchat_enabled]'>";
-		echo $this->html_form_option("Yes", $webchat_enabled);
-		echo $this->html_form_option("No", $webchat_enabled);
-		echo "</select>";
-	}
-
-  private function html_form_option($value, $selected) {
-    $option = '<option value="'.$value.'"'; 
-
-		if ($value == $selected) {
-			$option .= ' selected';
-		}
-
-	  $option .= '>'.$value.'</option>';
-
-    return $option;
+		echo "<input type=\"text\" name=\"broadly_options[broadly_account_id]\" value='$account_id'>";
 	}
 
 }
